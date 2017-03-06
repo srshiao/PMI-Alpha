9806bb41ed0c94fe9d4dd4c5e95beb88b4fdbbc5
JaredJRoss <jaredrossj@gmail.com>
2017-03-06 17:04:26 -0500

    trying to deploy app
    Former-commit-id: 2b00ec2be5683a879e905d1114c0d9362398fac0 [formerly 0ea849ffa1601fb2152865a130bcca72465ed51d]
    Former-commit-id: dba28adb64876cc6488bedc5df02ebedb227f798

--- pmi_alpha/pmi_alpha/settings.py
+++ pmi_alpha/pmi_alpha/settings.py
@@ -91,12 +91,12 @@ if 'RDS_DB_NAME' in os.environ:
         }
     }
 else:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
