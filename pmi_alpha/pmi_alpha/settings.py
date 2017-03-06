ea8c76ffac65e29f0740f72180400be441549554
JaredJRoss <jaredrossj@gmail.com>
2017-03-06 16:10:24 -0500

    Creating eb instance
    Former-commit-id: d99ba654ade23dd66564fdb4a0372772adf593c9 [formerly e313fb88ce99e604b266509092e2119756763f75]
    Former-commit-id: 7c800f82090e110d837d9eeab00bb6d5761c6417

--- pmi_alpha/pmi_alpha/settings.py
+++ pmi_alpha/pmi_alpha/settings.py
@@ -79,6 +79,18 @@ WSGI_APPLICATION = 'pmi_alpha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
