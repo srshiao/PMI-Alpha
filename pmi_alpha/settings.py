fc6f300424cb20f3662502aa1939f45751899225
JaredJRoss <jaredrossj@gmail.com>
2017-03-22 11:27:39 -0400

    added collect static and superuser
    Former-commit-id: 338a5dd75debe06c8e9421543a2545f6e959eb5b [formerly 31552390fd6645dfc58e0994998ac929b5bcc7d1]
    Former-commit-id: b52e5bc276cbaceb23087f2c0ca6c07056b4c499

--- pmi_alpha/settings.py
+++ pmi_alpha/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3%%glo(mp0z()b*@vnj-0a_liem!%$=&+3-nq%74u(xx!1m_&e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pmi-alpha.us-east-1.elasticbeanstalk.com', 'localhost']
+ALLOWED_HOSTS = ['pmi-alpha.us-east-1.elasticbeanstalk.com', 'localhost','127.0.0.1']
 
 
 # Application definition
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django_filters',       #Filter/Search tables
     'crispy_forms',         #Helps generate search/filter forms
     'bootstrap3',           #css purposes.
-    'database'
+    'database',
+    'pmi_alpha.images'
 ]
 
 MIDDLEWARE = [
