23aff374f03a19b5250e1b5e90c8de60d666caa0
JaredJRoss <jaredrossj@gmail.com>
2017-03-22 13:58:05 -0400

    static sucks
    Former-commit-id: 685954cd943ea42d0c5b210814192709465dacfc [formerly f99063630a96649961bf30b9f729517133d0cd43]
    Former-commit-id: 6d43aa9f41d3bc6f12d923f4b14180b9c3c33844

--- pmi_alpha/settings.py
+++ pmi_alpha/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/www/static/'
+STATIC_URL = '/static/'
 
 
 LOGIN_REDIRECT_URL = 'index'
@@ -146,7 +146,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,,'..','www', 'static')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
