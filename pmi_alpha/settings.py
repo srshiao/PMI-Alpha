657321b8c9063cad9035537711bd57c81ee52ff8
JaredJRoss <jaredrossj@gmail.com>
2017-03-22 16:38:59 -0400

    please
    Former-commit-id: 599bcc1003521c8db47d8274effc75feb25a0a2a [formerly 7728b43a00bb0878923f630c4f1f8d6268008473]
    Former-commit-id: 227ccaa2a28461fa58563d5450ef2d6b29188fb5

--- pmi_alpha/settings.py
+++ pmi_alpha/settings.py
@@ -138,13 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
 
-
 LOGIN_REDIRECT_URL = 'index'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'..','static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
