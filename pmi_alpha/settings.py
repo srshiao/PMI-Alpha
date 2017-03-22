c5ab5f106892a0470ee41adeecdb48624c603fb6
JaredJRoss <jaredrossj@gmail.com>
2017-03-22 13:48:19 -0400

    static stuff sucks
    Former-commit-id: caecae2005aeb230edf4778dd3deb6b5107e6785 [formerly 3baf28e840cb2385545c458eb649fc889cedfd5c]
    Former-commit-id: 9eaf32f33cedda0afbda82d194fbfbf12245efc8

--- pmi_alpha/settings.py
+++ pmi_alpha/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'www/static/'
 
 
 LOGIN_REDIRECT_URL = 'index'
