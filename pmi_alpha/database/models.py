145d076339f411192e1b321ff3b2aebf87d691cc
JaredJRoss <jaredrossj@gmail.com>
2017-03-23 16:22:20 -0400

    changed folders
    Former-commit-id: 98dfd9f590b0b9d858d34a644e5e5f05af44394a [formerly ac234430b404b96c19bd4a0b68b63e5992870dc2]
    Former-commit-id: 0a58e96d7ad981bf4d705f590ddc1efe954f9485

---
+++ pmi_alpha/database/models.py
@@ -0,0 +1,211 @@
+from __future__ import unicode_literals
+import datetime
+from django.db import models
+from django.utils.translation import gettext as _
+from django.forms import ModelForm
+
+
+# USE THIS TO ENABLE SCROLL DOWN SELECTION
+GENDERCHOICE = (
+	('M', 'Male'),
+	('F', 'Female'),
+	('O', 'Other'),
+	)
+class Vendors(models.Model):
+	ZipCode = models.CharField(_("Zip code"), max_length = 10, default = None)
+	TIN = models.IntegerField(_("TIN"), default = None)
+	State = models.CharField(_("State"), max_length = 10, default = None)
+	POC = models.CharField(_("Point of Contact"), max_length = 50, default = None)
+	Phone = models.CharField(_("Phone"), max_length = 20, default = None)
+	LegalName = models.CharField(_("Legal Name"), max_length = 50, default = None)
+	Fax = models.CharField(_("Fax"), max_length = 50, default = None)
+	Email = models.CharField(_("Email"), max_length = 50, default = None)
+	DUNs = models.CharField(_("DUNs"), max_length = 50, default = None)
+	DBA = models.CharField(_("DBA"), max_length = 50, default = None)
+	Country = models.CharField(_("Country"), max_length = 20, default = None)
+	City = models.CharField(_("City"), max_length = 20, default = None)
+	CAGE = models.CharField(_("CAGE"), max_length = 50, default = None)
+	Address = models.CharField(_("Address"), max_length = 50, default = None)
+
+class Employee(models.Model):
+	VendorID = models.ForeignKey(Vendors)
+	SubmittedViaWebform = models.BooleanField(_("Submitted Via Webform (T/F)"), default = True)
+	FName = models.CharField(_("Resource Last Name"), max_length = 20, default = None)
+	MName = models.CharField(_("Resource First Name"), max_length = 20, default = None)
+	LName = models.CharField(_("Resource First Name"), max_length = 20, default = None)
+	
+	CreatedBy = models.CharField(_("Created By"), max_length = 20, default = None)
+	Created = models.DateField(_("Created"), default=datetime.date.today)
+	RequestType = models.CharField(_("Request Type"), max_length = 20, default = None)
+	ClockSequence = models.CharField(_("Clock Sequence"), max_length = 20, default = None)
+	EffectiveDate = models.DateField(_("Effective Date of Change in Record"), default=datetime.date.today)
+	PayrollCompany = models.CharField(_("Payroll Company"), max_length = 20, default = None)
+	ProbationAlert = models.DateField(_("Probation Alert"), default=datetime.date.today)
+	ProjectFunctionalTitle = models.CharField(_("Project Functional Title"), max_length = 50, default = None)
+	GbSecruityAccessRequest = models.CharField(_("Gb Security Access Request"), max_length = 50, default = None)
+	OfferContractSent = models.BooleanField(_("Offer Contract Sent"), default = True)
+	OfferContractNDAFE = models.DateField(_("Offer Contract NDA FE"), default=datetime.date.today)
+	CommissionLogged = models.BooleanField(_("Commision Logged"), default = True)
+	CommissionDueDate = models.DateField(_("Commission Due Date"), default=datetime.date.today)
+	ToolKit = models.BooleanField(_("Tool Kit"), default = True)
+	SoftwareRequest = models.CharField(_("Software Request"), max_length = 50, default = None)
+	ClockSequenceHistoric = models.CharField(_("ClockSequenceHistoric"), max_length = 50, default = None)
+
+	Gender = models.CharField(_("Gender"), max_length = 10, choices=GENDERCHOICE, default = None)
+	PersonalEmail = models.CharField(_("Personal Email"), max_length = 50, default = None)
+	PMIEmail = models.CharField(_("PMI Email"), max_length = 50, default = None)
+	TypeOfContract = models.CharField(_("Type of Contract"), max_length = 20, default = None)
+	Addresses = models.CharField(_("Addresses"), max_length = 30, default = None)
+	City = models.CharField(_("City"), max_length = 20, default = None)
+	ZipCode = models.CharField(_("Zip code"), max_length = 10, default = None)
+	State = models.CharField(_("State"), max_length = 10, default = None)
+	Country = models.CharField(_("Country"), max_length = 20, default = None)
+	Phone = models.CharField(_("Phone"), max_length = 20, default = None)
+	BirthDate = models.DateField(_("Birth Date"), default=datetime.date.today)
+	DateOfHire = models.DateField(_("Date of Hire"), default=datetime.date.today)
+	PMICareerTitle = models.CharField(_("PMI Career Title    "), max_length = 50, default = None)
+	ResourceType= models.CharField(_("Resource Type"), max_length = 20, default = None)
+	GSALaborCategory = models.CharField(_("GSA Labor Category"), max_length=20, default = None)
+	VacationHour = models.CharField(_("Vacation Hour"), max_length = 20, default = None)
+	SickLeaveHour = models.CharField(_("Sick Leave Hour"), max_length = 20, default = None)
+	EmployementStatus = models.CharField(_("Employment Status"), max_length = 50, default = None)
+	ContractLaborCategory = models.CharField(_("Contract Labor Category"), max_length = 20, default = None)
+	RecruitmentSource = models.CharField(_("Recruitment Source"), max_length = 50, default = None)
+	ResponsibleRecruiter = models.CharField(_("Responsible Recruiter"), max_length = 50, default = None)
+	HUBzone = models.BooleanField(_("HUBzone"), default = True)
+	ProbationaryEnd= models.DateField(_("Probationary End"), default=datetime.date.today)
+	TerminationDate = models.DateField(_("Termination Date"), default=datetime.date.today)
+	Department = models.CharField(_("Department"), max_length = 50, default = None)
+	WorkLocation = models.CharField(_("Work Location"), max_length = 50, default = None)
+	Billable = models.BooleanField(_("Billable"), default = True)
+	SalaryType = models.CharField(_("Salary Type"), max_length = 20, default = None)
+	Salary = models.IntegerField(_("Salary"), default = None)
+	ClearanceStatus = models.CharField(_("Clearance Status"), max_length = 20, default = None)
+	ClearedDate = models.DateField(_("Cleared Date"), default=datetime.date.today)
+	ClearedType = models.CharField(_("Cleared Type"), max_length = 20, default = None)
+	Rehire = models.BooleanField(_("Rehire (T/F)"), default = True)
+	_401KEligible = models.BooleanField(_("401K Eligible"), default = True)
+	OrientationDate = models.DateField(_("Orientation Date"), default=datetime.date.today)
+	ClientBillRate = models.CharField(_("Client Bill Rate"), max_length = 20, default = None)
+	GSA_vehicle = models.CharField(_("GSA Vehicle"), max_length = 50, default = None)
+	GSA_rate = models.CharField(_("GSA Rate"), max_length = 50, default = None)
+	CAMPINSent = models.CharField(_("CAMPINSent"), max_length = 50, default = None)
+	PaycomLogin = models.CharField(_("Paycom Login"), max_length = 50, default = None)
+	eFAACT = models.CharField(_("eFAACT"), max_length=50, default = None)
+	Computer = models.CharField(_("Computer"), max_length=50, default = None)
+	BusinessCard = models.CharField(_("Business Card"), max_length=50, default = None)
+	Comments = models.CharField(_("Comments"), max_length = 1000, default = None)
+
+
+class GoogleGroup(models.Model):
+	Name = models.CharField(_("Name"), max_length = 50, default = None)
+	Admin = models.CharField(_("Admin"), max_length = 50, default = None)
+
+class Customer(models.Model):
+	LegalName = models.CharField(_("Legal Name"), max_length = 50, default = None)
+	DBA = models.CharField(_("DBA"), max_length = 50, default = None)
+	Address = models.CharField(_("Address"), max_length = 50,  default = None)
+	City = models.CharField(_("City"), max_length = 20, default = None)
+	ZipCode = models.CharField(_("Zip code"), max_length = 10, default = None)
+	State = models.CharField(_("State"), max_length = 10, default = None)
+	Country = models.CharField(_("Country"), max_length = 20, default = None)
+	Phone = models.CharField(_("Phone"), max_length = 20, default = None)
+	LegalName = models.CharField(_("Legal Name"), max_length = 50, default = None)
+	Fax = models.CharField(_("Fax"), max_length = 50, default = None)
+	Email = models.CharField(_("Email"), max_length = 50, default = None)
+	DUNs = models.CharField(_("DUNs"), max_length = 9, default = None)
+	CAGE = models.CharField(_("CAGE"), max_length = 50, default = None)
+	POC = models.CharField(_("Point of Contact"), max_length = 50, default = None)
+	TIN = models.CharField(_("TIN"), max_length=11, default = None)
+
+class Contract(models.Model):
+	CustomerID = models.ForeignKey(Customer)
+	IssuingCompany = models.CharField(_("Issuing Company"), max_length = 50, default = None)
+	ContractNumber = models.CharField(_("Contract Number"), max_length = 50, default = None)
+	DocumentLocation = models.CharField(_("Document Location"), max_length = 50, default = None)
+	OrganizationType = models.CharField(_("Organization Type"), max_length = 50, default = None)
+	POC = models.CharField(_("Point of Contact"), max_length = 50, default = None)
+	EffectiveDate = models.DateField(_("Effective Date"), default=datetime.date.today)
+	EndDate = models.DateField(_("End Date"), default=datetime.date.today)
+	StartDate = models.DateField(_("Start Date"), default=datetime.date.today)
+	Status = models.CharField(_("Status"), max_length = 50, default = None)
+	Comments = models.CharField(_("Comments"), max_length = 1000, default = None)
+
+class Partner(models.Model):
+	Address = models.CharField(_("Address"), max_length = 50, default = None)
+	CAGE = models.CharField(_("CAGE"), max_length = 50, default = None)
+	City = models.CharField(_("City"), max_length = 20, default = None)
+	ZipCode = models.CharField(_("Zip code"), max_length = 10, default = None)
+	State = models.CharField(_("State"), max_length = 10, default = None)
+	Country = models.CharField(_("Country"), max_length = 20, default = None)
+	Phone = models.CharField(_("Phone"), max_length = 20, default = None)
+	LegalName = models.CharField(_("Legal Name"), max_length = 50, default = None)
+	Fax = models.CharField(_("Fax"), max_length = 50, default = None)
+	Email = models.CharField(_("Email"), max_length = 50, default = None)
+	DBA = models.CharField(_("DBA"), max_length = 50, default = None)
+	DUNs = models.CharField(_("DUNs"), max_length = 9, default = None)
+	POC = models.CharField(_("Point of Contact"), max_length = 50, default = None)
+	TIN = models.CharField(_("TIN"), max_length=11, default = None)
+	Type = models.CharField(_("Type"), max_length=20, default = None)
+
+class Department(models.Model):
+	ContractID = models.ForeignKey(Contract)
+	CustomerID = models.ForeignKey(Customer)
+	Name = models.CharField(_("Name"), max_length = 50, default = None)
+	Location = models.CharField(_("Location"), max_length = 50, default = None)
+	Fax = models.CharField(_("Fax"), max_length = 50, default = None)
+	Supervisor = models.CharField(_("Supervisor"), max_length = 50, default = None)
+	Phone = models.CharField(_("Phone"), max_length = 50, default = None)
+
+class Department_Employee(models.Model):
+	DepartmentID = models.ForeignKey(Department)
+	ContractID = models.ForeignKey(Contract)
+	CustomerID = models.ForeignKey(Customer)
+	EmployeeID = models.ForeignKey(Employee)
+	VendorID = models.ForeignKey(Vendors)
+class Contract_Employee(models.Model):
+	ContractID = models.ForeignKey(Contract)
+	CustomerID = models.ForeignKey(Customer)
+	EmployeeID = models.ForeignKey(Employee)
+	VendorID = models.ForeignKey(Vendors)
+
+class Customer_Vendor(models.Model):
+	CustomerID = models.ForeignKey(Customer)
+	VendorID = models.ForeignKey(Vendors)
+
+class Customer_Employee(models.Model):
+	CustomerID = models.ForeignKey(Customer)
+	EmployeeID = models.ForeignKey(Employee)
+	VendorID = models.ForeignKey(Vendors)
+
+class Customer_Partner(models.Model):
+	CustomerID = models.ForeignKey(Customer)
+	PartnerID = models.ForeignKey(Partner)
+
+class POC(models.Model):
+	PartnerID = models.ForeignKey(Partner)
+	ContractID = models.ForeignKey(Contract)
+	CustomerID = models.ForeignKey(Customer)
+	Address = models.CharField(_("Address"), max_length = 50)
+	Phone = models.CharField(_("Phone"), max_length = 20)
+	Email = models.CharField(_("Email"), max_length = 50)
+	LName = models.CharField(_("Resource First Name"), max_length = 20)
+	FName = models.CharField(_("Resource Last Name"), max_length = 20)
+
+class Vendor_Contract(models.Model):
+	CustomerID = models.ForeignKey(Customer)
+	ContractID = models.ForeignKey(Contract)
+	VendorID = models.ForeignKey(Vendors)
+
+class GoogleGroup_Employee(models.Model):
+	GoogleGroupID = models.ForeignKey(GoogleGroup)
+	EmployeeID = models.ForeignKey(Employee)
+	VendorID = models.ForeignKey(Vendors)
+
+
+
+
+
+
+
+	
