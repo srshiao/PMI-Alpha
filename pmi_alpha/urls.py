ba39c8d0894a793efaf35946ee054c94e3626164
JaredJRoss <jaredrossj@gmail.com>
2017-03-23 13:17:52 -0400

    added debugging
    Former-commit-id: 2298bc9df2dbf3274482ad9ded5e6517e9ff5f50 [formerly 5965932726e6d539892f038b22f9b6b8c46c6ed3]
    Former-commit-id: 27d20af90003de1f61ac41dd94cc74f634b3977e

--- pmi_alpha/urls.py
+++ pmi_alpha/urls.py
@@ -18,8 +18,9 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^database/', include('database.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
