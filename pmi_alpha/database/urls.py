ea998aad0b597142ba99de07c32c20afbf96578f
intherenzone <maviles@umd.edu>
2017-04-27 10:53:14 -0400

    Revert "Reverted changes"
    This reverts commit 2bb1f2f4b5f30ab544e02011f75342f89cf335e3 [formerly f33504b202faa2eaa8d25651ff6ea6e772a0480c] [formerly be91dea8e351a32fe53e6b3b2502e2579b29a77e [formerly 91da55a7beef11acc697d5d9888b731ab2440b6b]].
    
    
    Former-commit-id: cad76042299be8cae10dc473dced97c12ceb2f27 [formerly ff7a3ff8d50352af1c5f008135f27a7e542bd74a]
    Former-commit-id: fceea394ca1570b1fdf0f82892aa1edb90e7ccba

--- pmi_alpha/database/urls.py
+++ pmi_alpha/database/urls.py
@@ -30,7 +30,10 @@ urlpatterns = [
 
 
    #BASIC SEARCH
-   url(r'^search/', include("watson.urls", namespace="watson"), {'template_name' : 'database/search_results.html',}),
+   url(r'^search/', include("watson.urls", namespace="basic"), {'template_name' : 'database/search_results.html', 'paginate_by' : '10'}),
+   #AVANCED SEARCH
+   url(r'^search/', include("watson.urls", namespace="advanced"), 
+         {'template_name' : 'database/search_results.html', 'models': [Employee] , 'paginate_by' : '10'}),
 
 
    #USD WITH SIDE BAR TO REDIRECT
