476d2c3cc34abb5bad2a7e03c2215b2c189ca18c
JaredJRoss <jaredrossj@gmail.com>
2017-12-05 14:53:20 -0500

    clockin changes
    Former-commit-id: a443a1dec7bd38dc5ba15c16a90df3127669a83d [formerly ca40ad2a417cc25a73b41a66f81d90141e3217dd]
    Former-commit-id: 1809c2dfa40cb1dfdbddacb19fe74d5a05599054

--- pmi_alpha/clockin/urls.py
+++ pmi_alpha/clockin/urls.py
@@ -12,14 +12,16 @@ urlpatterns = [
     url(r'^current_session/$', views.crt_session, name= 'my_session'),
     url(r'^start_shift/$', views.add_new, name= 'add'),
     url(r'^edit/(?P<work_id>\d+)/$', views.clockout, name='end_work_session'),
-    url(r'^delete/(?P<pk>\d+)/$', views.workDelete.as_view(model = Work), name="work_delete"),
-    url(r'^adminhome/$', views.sendmail, name= 'adminhome'),
+    url(r'^admin_edit/(?P<pk>\d+)/delete/$', views.workDelete.as_view(model = Work), name="work_delete"),
+    url(r'^adminhome/$', views.adminhome, name= 'adminhome'),
     url(r'^logout/$', views.logout_page, name = 'logout'),
     url(r'^history/$', views.past_time, name= 'past'),
     url(r'^admin_edit/(?P<work_id>\d+)/$', views.edit_hours, name='edit_hours'),
     url(r'^new_hours/$', views.add_work, name= 'add_work'),
     url(r'^intern-autocomplete/$',views.InternAutocomplete.as_view(),name='intern-autocomplete',),
+	url(r'^active_interns/$', views.all_active, name= 'active_interns'),
     url(r'^adminhome/intern_detail/$', TemplateView.as_view(template_name='timesheet/intern_detail.html'), name='intern_detail'),
+
     
 
 
--- pmi_alpha/clockin/views.py
+++ pmi_alpha/clockin/views.py
@@ -26,10 +26,7 @@ from .config import *
 from django.core.mail import send_mail,EmailMultiAlternatives,EmailMessage
 from django.db.models.functions import Concat
 from django.db.models import Count
-#from django.core.mail.MIMEMultipart import MIMEMultipart
-#		from email.MIMEText import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+
 
 
 def logout_page(request):
@@ -60,7 +57,7 @@ def work_list(request):
 
 	return render(request, 'timesheet/active_work_sessions.html', context)
 
-#TGENERATES CURRENT SESSION PAGE. TABLE. 
+#TGENERATES CURRENT SESSION PAGE. TABLE.
 @login_required
 def crt_session(request):
 	filter = Work.objects.filter(user=request.user).filter(active_session=True)
@@ -84,19 +81,55 @@ def crt_session(request):
 
 	return render(request, 'timesheet/current_session.html', context)
 
-
+def all_active(request):
+	if not request.user.is_superuser:
+		return HttpResponseRedirect('/clockin/')
+	filter = Work.objects.filter(active_session=True)
+	context = {
+		'filter':filter,
+	}
+	
+	return render(request, 'timesheet/all_active_sessions.html', context)
 
 @login_required
 def past_time(request):
 	filter1 = Work.objects.filter(user=request.user).filter(active_session=False)
-
 	intern_obj = Intern.objects.filter(username = request.user)
+	userid = intern_obj[0].id
 	name = intern_obj.first()
-
+	form = PastLogsForm(request.POST or None)
+	exp = Work.objects.all()
+	exp=exp.filter(intern__exact=userid)
 	context = {
-		'filter1':filter1,
 		'name' : name,
 	}
+	if form.is_valid():
+		year = int(form.cleaned_data['year'])
+		month = int(form.cleaned_data['month'])
+		pay_period = form.cleaned_data['pay_period']
+		context['year'] = year
+		month_name = calendar.month_name[month]
+		context['month'] = month_name
+		context['pay_period'] = pay_period
+
+		if month in range(1,13) and year in range(2015,datetime.date.today().year+1):
+			if pay_period=='First Pay Period':
+				start_date = datetime.date(year,month,1)
+				end_date = datetime.date(year,month,15)
+			elif pay_period=='Second Pay Period':
+				start_date = datetime.date(year, month, 16)
+				end_date = datetime.date(year, month, calendar.monthrange(year,month)[1])
+			else:
+				start_date = datetime.date(year, month, 1)
+				end_date = datetime.date(year, month, calendar.monthrange(year, month)[1])
+			exp = exp.filter(date__range=(start_date, end_date))
+		elif year in range(2015,(datetime.date.today().year)+1):
+			start_date = datetime.date(year, 1, 1)
+			end_date = datetime.date(year, 12, 31)
+			exp = exp.filter(date__range=(start_date, end_date))
+
+	context['form'] = form
+	context['exp'] = exp
 
 	return render(request, 'timesheet/past_time.html', context)
 
@@ -129,24 +162,25 @@ def add_new(request):
 def clockout(request, work_id):
 	instance = get_object_or_404(Work, id=work_id)
 	form = ClockoutForm(request.POST or None, instance=instance)
-	intern_obj = Intern.objects.filter(username = request.user)
+	intern_obj = Intern.objects.filter(username=request.user)
 	name = intern_obj.first()
+	if (instance.active_session == False):
+		return HttpResponseRedirect('/clockin/')
 
 	if form.is_valid():
 		obj = form.save(commit=False)
 		obj.time_out = datetime.datetime.now().time()
 		obj.active_session = False
 		my_date = datetime.date.today()
-
 		delta = datetime.datetime.combine(my_date,obj.time_out) - datetime.datetime.combine(my_date,obj.time_in)
 	
 		totalseconds = delta.total_seconds()
 		hours = totalseconds/3600
-		if hours > 9:
+		if hours > 8:
 			obj.duration = 0
 		elif hours < 0:
 			new_hours = hours+24
-			if new_hours > 9:
+			if new_hours > 8:
 	 			new_hours = 0
 			obj.duration = new_hours 
 		else:
@@ -168,6 +202,8 @@ def clockout(request, work_id):
 def edit_hours(request,work_id):
 	if not request.user.is_superuser:
 		return HttpResponseRedirect('/clockin/')
+	intern_obj = Intern.objects.filter(username = request.user)
+	name = intern_obj.first()
 
 	instance = get_object_or_404(Work, id=work_id)
 	form = WorkForm(request.POST or None, instance=instance)
@@ -181,7 +217,7 @@ def edit_hours(request,work_id):
 		delta = datetime.datetime.combine(my_date,obj.time_out) - datetime.datetime.combine(my_date,obj.time_in)
 		totalseconds = delta.total_seconds()
 		hours = totalseconds/3600
-		if hours > 9:
+		if hours > 8:
 			obj.duration = 0
 		elif hours < 0:
 			new_hours = hours+24
@@ -192,9 +228,11 @@ def edit_hours(request,work_id):
 		obj.duration = round(obj.duration*4)/4
 		obj.save()
 
-		return HttpResponseRedirect('/clockin/adminhome')
+		return HttpResponseRedirect('/clockin/adminhome/')
 	context = {
 		'form' : form,
+		'name' : name,
+		'token' : SLACK_BOT_TOKEN,
 		'pk' : work_id
 	}
 
@@ -205,16 +243,36 @@ class workDelete(DeleteView):
 	success_url = reverse_lazy('adminhome')
 	template_name = 'timesheet/delete_work_session.html'
 
+	def get_context_data(self, **kwargs):
+		# Call the base implementation first to get a context
+		context = super(workDelete, self).get_context_data(**kwargs)
+		# Add in a QuerySet of all the books
+		context['token'] = SLACK_BOT_TOKEN
+		return context
+
 
 @login_required
 #Clock in function
 def add_work(request):
+	if not request.user.is_superuser:
+		return HttpResponseRedirect('/clockin/')
+	intern_obj = Intern.objects.filter(username = request.user)
+	name = intern_obj.first()
+	
 	form = WorkForm(request.POST or None);
+	obj = form.save(commit=False)
 	context = {
-		'form' : form
+		'form' : form,
+		'token' : SLACK_BOT_TOKEN,
+		'name' : name,
 	}
 	if form.is_valid():
-		obj = form.save(commit=False)
+		context = {
+		'form' : form,
+		'token' : SLACK_BOT_TOKEN,
+		'name' : name,
+		'user' : request.user,
+	}
 		obj.user = obj.intern.username
 		obj.active_session = False
 
@@ -245,55 +303,62 @@ class InternAutocomplete(autocomplete.Select2QuerySetView):
 
 #for implementing report generation functionality
 @login_required
-def sendmail(request):
+def adminhome(request):
 	if not request.user.is_superuser:
 		return HttpResponseRedirect('/clockin/')
-	form = InternSummaryForm(request.POST or None)
+	year=int(0)
+	month=int(0)
+	pay_period=""
+	form = InternSummaryForm(request.GET or None)
+
 	exp = Work.objects.all()
+
+	print(form.is_valid())
 	if form.is_valid():
 		obj = form.save(commit=False)
 		if obj.intern:
 			user = obj.intern.username
-			exp= Work.objects.filter(user__contains=user)
-		year = datetime.date.today().year
+			exp= Work.objects.filter(user__exact=user)
+		year = int(form.cleaned_data['year'])
 		month = int(form.cleaned_data['month'])
 		pay_period = form.cleaned_data['pay_period']
 		#email = form.cleaned_data['email']
-		if month in range(1,12):
+		if month in range(1,13) and year in range(2015,datetime.date.today().year+1):
 			if pay_period=='First Pay Period':
 				start_date = datetime.date(year,month,1)
 				end_date = datetime.date(year,month,15)
-				exp = exp.filter(date__range=(start_date,end_date))
 			elif pay_period=='Second Pay Period':
 				start_date = datetime.date(year, month, 16)
 				end_date = datetime.date(year, month, calendar.monthrange(year,month)[1])
-				exp = exp.filter(date__range=(start_date, end_date))
-			elif pay_period=='Both Pay Periods':
+			else:
 				start_date = datetime.date(year, month, 1)
 				end_date = datetime.date(year, month, calendar.monthrange(year, month)[1])
-				exp = exp.filter(date__range=(start_date, end_date))
+			exp = exp.filter(date__range=(start_date, end_date))
+		elif year in range(2015,(datetime.date.today().year)+1):
+			start_date = datetime.date(year, 1, 1)
+			end_date = datetime.date(year, 12, 31)
+			exp = exp.filter(date__range=(start_date, end_date))
+
 
 	exp1 = exp.values('intern_id','intern__FName','intern__LName').annotate(total=Sum('duration'))
-	if request.POST.get('myemail'):
+
+	if request.GET.get('myemail'):
+		html_message = loader.render_to_string('timesheet/get_report.html', {'exp':exp1})
 		email = form.cleaned_data['email']
-		html_message = loader.get_template('timesheet/get_report.html').render({'exp':exp1})
-		send_mail('Test email', 'message', 'PMIClockin@gmail.com', [email],html_message=html_message)
+		send_mail('Intern Hours Summary', 'message', 'PMIClockin@gmail.com', [email], html_message=html_message)
 
 	if request.POST.get('mybtn1'):
 		che=request.POST.get('mybtn1')
 		exp=exp.filter(intern__exact=che)
 		month_name = calendar.month_name[month]
-		if month:
-			return render(request, 'timesheet/intern_detail.html', context={'exp': exp,'pay_period':pay_period,'month':month_name})
-		else:
-			return render(request, 'timesheet/intern_detail_all.html', context={'exp': exp})
+		return render(request, 'timesheet/intern_detail.html', context={'exp': exp,'pay_period':pay_period,'month':month_name,'year':year})
+
 	if request.POST.get('mybtn'):
 		ch = request.POST.get('checkbox','')
 		if not ch == '':
 			url = reverse_lazy ('edit_hours', kwargs = {'work_id':ch})
 			return HttpResponseRedirect(url)
 
-	return render(request, 'timesheet/email.html',context = {'form': form,'exp':exp1})
-
+	return render(request, 'timesheet/admin_home.html',context = {'form': form,'exp':exp1})
 
 
