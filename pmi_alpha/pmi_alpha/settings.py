eb3cf1dbeb5c4af615aae2f1824e4167ca505894
Jared Ross <jaredrossj@gmail.com>
2018-02-19 16:43:11 -0500

    easier intern

--- pmi_alpha/pmi_alpha/settings.py
+++ pmi_alpha/pmi_alpha/settings.py
@@ -115,12 +115,9 @@ if 'RDS_DB_NAME' in os.environ:
         }
     }
 else:
-    DATABASES = {
-        'default': {
-            if 1==1:
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-            else:
+    if 1==0:
+        DATABASES = {
+            'default': {
                 'ENGINE': 'django.db.backends.mysql',
                 'NAME':'django_db',
                 'USER': 'root',
@@ -130,9 +127,15 @@ else:
                 'OPTIONS': {
                     'sql_mode': 'traditional',
                 }
+            }
         }
-    }
-
+    else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
