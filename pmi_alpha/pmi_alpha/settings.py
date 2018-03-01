4a207c9b801118da2b38c17023ea2159d080fd92
Jared Ross <jaredrossj@gmail.com>
2018-03-01 14:04:10 -0500

    database error

--- pmi_alpha/pmi_alpha/settings.py
+++ pmi_alpha/pmi_alpha/settings.py
@@ -106,7 +106,7 @@ if 'RDS_DB_NAME' in os.environ:
             'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'PASSWORD': 'paradyme',
             'HOST': os.environ['RDS_HOSTNAME'],
             'PORT': os.environ['RDS_PORT'],
             'OPTIONS': {
